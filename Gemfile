719a00bfdeeecf75f8ade512d1526553201b410e
Ivan Lucev <lucev.ivan@gmail.com>
2012-02-01 04:22:15 +0100

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.1'
+
+gem 'haml'
+gem 'haml-rails', :group => :development
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1' 
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
+gem 'execjs'
+gem 'therubyracer'
+
+group :development, :test do
+  gem 'sqlite3'
+end
+
+group :test do
+  gem 'rspec-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mitzyk::Application.config.secret_token = '5f25b7aba1b7c92066f564197947daeb6913ff88742318e6cf62f019e0d74ef1c09397b6e91061aabb399a2c6361039da38f7fca3773ef9a9a5877941a342c7f'
