f8ddab69cd862318b558ac86d7fc633acd127ae9
Ivan Lucev <lucev.ivan@gmail.com>
2012-02-05 13:15:17 +0100

    Fixed first day of week calculating

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # First day of week, assuming that week begins on Monday
   def first_of_week
-    Date.today - Date.today.wday + 1
+    Date.today - (Date.today-1).wday
   end
   
   def first_of_month
