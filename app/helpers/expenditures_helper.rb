95fa47d94e1b1d646184c50d204187ec937226f4
Ivan Lucev <lucev.ivan@gmail.com>
2012-02-05 13:21:35 +0100

    Fixed expenditures status calculation

--- app/helpers/expenditures_helper.rb
+++ app/helpers/expenditures_helper.rb
@@ -8,9 +8,10 @@ module ExpendituresHelper
     tolerance = 0.1
     expenditures = current_user.spent_this_week
     
-    if expenditures < (current_user.weekly_limit/7)*Date.today.wday*(1-tolerance)
+    # assuming that first day of week is Monday, week day is 1 based instead of 0 based
+    if expenditures < (current_user.weekly_limit/7)*((Date.today-1).wday+1)*(1-tolerance)
       return 'green'
-    elsif expenditures > (current_user.weekly_limit/7)*Date.today.wday*(1+tolerance)
+    elsif expenditures > (current_user.weekly_limit/7)*((Date.today-1).wday+1)*(1+tolerance)
       return 'red'
     end
     else return 'yellow'
