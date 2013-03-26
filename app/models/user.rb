01f4d060803370ce9bed1baf408ea5ad329df6a9
Ivan Lučev <lucev.ivan@gmail.com>
2013-03-26 12:13:01 +0100

    changed expenditures order

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,7 +45,7 @@ class User < ActiveRecord::Base
   
   def range_expenditures(start_date, end_date)
     Expenditure.where("user_id = ? AND date >= ? AND date <= ?",
-      self.id, start_date, end_date).order("date desc")
+      self.id, start_date, end_date).order("date DESC, created_at DESC")
   end
   
   def range_expenditures_sum(start_date, end_date)
