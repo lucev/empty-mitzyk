bcd80f8efe105dc93e8886124b0153dd73ed41c5
Ivan Lučev <lucev.ivan@gmail.com>
2016-03-02 16:41:24 +0100

    Refactor user expenditures sum

--- app/models/expenditure.rb
+++ app/models/expenditure.rb
@@ -6,6 +6,7 @@ class Expenditure < ActiveRecord::Base
   validates_presence_of :amount
   validates_presence_of :date
 
+  scope :period, -> start_at, end_at { where("date >= ? AND date <= ?", start_at, end_at) }
   scope :period_start,  -> value { where("date >= ?", value) }
   scope :period_end,    -> value { where("date <= ?", value) }
   scope :category,      -> value { where("category_id = ?", value) }
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,13 +13,13 @@ class User < ActiveRecord::Base
   has_many :expenditures
   
   def week_expenditures
-    Expenditure.where("user_id = ? AND date >= ? AND ofteness = ?",
-      self.id, first_of_week, 'daily')
+    expenditures.where("ofteness = ?", 'daily').
+      period(Date.today.beginning_of_week, Date.today.end_of_week)
   end
   
   def month_expenditures
-    Expenditure.where("user_id = ? AND date >= ? AND (ofteness = ? OR ofteness = ?)",
-      self.id, first_of_month, 'daily', 'monthly')
+    expenditures.where("ofteness = ? OR ofteness = ?", 'daily', 'monthly').
+      period(Date.today.beginning_of_month, Date.today.end_of_month)
   end
   
   def month_percentage
@@ -28,19 +28,11 @@ class User < ActiveRecord::Base
   end
 
   def spent_this_week
-    sum = (0.00).to_d
-    self.week_expenditures.each do |expenditure|
-      sum += expenditure.amount
-    end
-    return sum
+    week_expenditures.sum(:amount)
   end
   
   def spent_this_month
-    sum = (0.00).to_d
-    self.month_expenditures.each do |expenditure|
-      sum += expenditure.amount
-    end
-    return sum
+    month_expenditures.sum(:amount)
   end
 
   def categories_last_position
