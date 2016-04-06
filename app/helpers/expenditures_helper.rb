f83e3b670d8d4ea01032b109bc41818e8a9e5d9a
Ivan Lučev <ivan.lucev@refactorit.co>
2016-04-06 20:38:12 +0200

    Improve code style

--- app/helpers/expenditures_helper.rb
+++ app/helpers/expenditures_helper.rb
@@ -1,16 +1,9 @@
 module ExpendituresHelper
 
   def weekly_spending_status
-    if current_user.weekly_limit.nil?
-      return ''
-    end
-    
-    if current_user.spent_this_week < current_user.lower_tolerance
-      return 'green'
-    elsif current_user.spent_this_week > current_user.upper_tolerance
-      return 'red'
-    end
-    else return 'yellow'
+    return '' if current_user.weekly_limit.nil?
+    return 'green' if current_user.spent_this_week < current_user.lower_tolerance
+    return 'red' if current_user.spent_this_week > current_user.upper_tolerance
+    'yellow'
   end
-  
 end
