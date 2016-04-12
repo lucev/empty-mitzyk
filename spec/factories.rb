b3653da111ad6733f6af655c1fbf951b140bd931
Ivan Lučev <ivan.lucev@refactorit.co>
2016-04-12 19:55:52 +0200

    Use factories in specs

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,4 +2,14 @@ FactoryGirl.define do
   factory :user do
     sequence(:email) { |n| "user_#{n}@example.com" }
   end
+
+  factory :expenditure do
+    amount '3.99'
+    date Date.today
+  end
+
+  factory :category do
+    name 'transportation'
+    position 1
+  end
 end
