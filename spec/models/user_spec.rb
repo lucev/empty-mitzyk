fc06e9f6d5d6012807eea6d3deb8f39616b2e183
Ivan Lučev <ivan.lucev@refactorit.co>
2016-04-19 19:29:43 +0200

    Use factory in expenditure spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe User do
+  let(:user) { FactoryGirl.create(:user) }
   it 'can be instantiated' do
     User.new.should be_an_instance_of(User)
   end
@@ -11,24 +12,15 @@ describe User do
   end
   
   it 'calculates this week expenditures' do
-    user = User.create!(:email => 'foo@example.com')
-    
-    expenditure_first = Expenditure.create!(:amount => 5.99, :date => Date.today - Date.today.wday+2,
-                                            :ofteness => 'daily')
-    expenditure_first.user = user
-    expenditure_first.save
-    
-    expenditure_second = Expenditure.create!(:amount => 2, :date => Date.today - Date.today.wday+3,
-                                             :ofteness => 'daily')
-    expenditure_second.user = user
-    expenditure_second.save
+    Expenditure.create!(amount: 5.99, date: Date.today - Date.today.wday + 2,
+                        ofteness: 'daily', user: user)
+    Expenditure.create!(amount: 2, date: Date.today - Date.today.wday + 3,
+                        ofteness: 'daily', user: user)
     
     user.spent_this_week.should == 7.99
   end  
 
   it 'has categories last position' do
-    user = FactoryGirl.create(:user)
-
     category_travel = Category.create(:name => 'Travel', :position => 1)
     category_travel.user = user
     category_travel.save
