da94db2f60045eb8e3d2bdac2e54987e13baeeea
Ivan Lučev <ivan.lucev@refactorit.co>
2017-04-20 14:51:56 +0200

    Add default scope for categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -5,6 +5,7 @@ class Category < ActiveRecord::Base
 
   validates_presence_of :name
 
+  default_scope { order(:created_at) }
   scope :active, -> { where(active: true) }
   
   def belongs_to? user
